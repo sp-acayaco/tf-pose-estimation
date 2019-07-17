045d95b29e1401e9a7f9c7df43922e011f3110ec
Adrian Cayaco <acayaco@stratpoint.com>
2019-07-17 15:09:17 +0800

    Updated sample code to run video.
    - Added video orientation check and auto rotation.
    - Updated instantiation of the model and predictor which was taken from the run.py

--- run_video.py
+++ run_video.py
@@ -4,7 +4,9 @@ import time
 
 import cv2
 import numpy as np
+import ffmpeg
 
+from tf_pose import common
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 
@@ -18,30 +20,71 @@ logger.addHandler(ch)
 
 fps_time = 0
 
+def check_rotation(path_video_file):
+    # this returns meta-data of the video file in form of a dictionary
+    meta_dict = ffmpeg.probe(path_video_file)
+
+    # from the dictionary, meta_dict['streams'][0]['tags']['rotate'] is the key
+    # we are looking for
+    rotateCode = None
+    
+    tags = meta_dict['streams'][0]['tags']
+    rotation = tags.get('rotate')
+    
+    if rotation:
+        if int(rotation) == 90:
+            rotateCode = cv2.ROTATE_90_CLOCKWISE
+        elif int(rotation) == 180:
+            rotateCode = cv2.ROTATE_180
+        elif int(rotation) == 270:
+            rotateCode = cv2.ROTATE_90_COUNTERCLOCKWISE
+            
+    return rotateCode
+    
+def correct_rotation(frame, rotateCode):  
+    return cv2.rotate(frame, rotateCode)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
     parser.add_argument('--video', type=str, default='')
-    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
+    parser.add_argument('--resolution', type=str, default='1312x736', help='network input resolution. default=1312x736')
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
     parser.add_argument('--show-process', type=bool, default=False,
                         help='for debug purpose, if enabled, speed for inference is dropped.')
     parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
     args = parser.parse_args()
 
     logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
+    
     w, h = model_wh(args.resolution)
     e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    
     cap = cv2.VideoCapture(args.video)
+    
+    # check if video requires rotation
+    rotateCode = check_rotation(args.video)
 
     if cap.isOpened() is False:
         print("Error opening video stream or file")
     while cap.isOpened():
+    
         ret_val, image = cap.read()
+        
+        # check if the frame needs to be rotated
+        if rotateCode is not None:
+            image = correct_rotation(image, rotateCode)
+            
+        #image = cv2.resize(image,(432,368))
 
-        humans = e.inference(image)
+        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+        
         if not args.showBG:
             image = np.zeros(image.shape)
+            
+        #print(humans)
+            
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
 
         cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
